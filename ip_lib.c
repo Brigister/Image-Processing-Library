fd5682764dd5773fd0d38f02b5135725423d0577
Simone Zanon <eyeleren@live.it>
2020-05-08 15:31:14 +0200

    sub+sum v2 + concat + subset + free + create(sistemata)
    Free da testare con valgrind.
    Concat implementata con controlli.
    subset implementata con controlli
    sub+sum modificate con controlli, rimane il dubbio sulla modifica/creazione di matrice.
    sistemata create

--- ip_lib.c
+++ ip_lib.c
@@ -126,49 +126,75 @@ ip_mat *ip_mat_create(unsigned int h, unsigned int w, unsigned int k, float v)
     nuova->h = h;
     nuova->k = k;
 
-
     /*allocamento stat*/
-  
+
     stats *stat = (stats *)malloc(sizeof(stats) * k);
     nuova->stat = stat;
 
     /*qualcosa*/
-    float*** A = (float***)malloc(h * sizeof(float**));
+    float ***data = (float ***)malloc(h * sizeof(float **));
 
-	if (A == NULL) {
-		fprintf(stderr, "Out of memory");
-		exit(1);
-	}
+    if (data == NULL)
+    {
+        printf("Out of memory");
+        exit(1);
+    }
 
-	for (int i = 0; i < h; i++)
-	{
-		A[i] = (float**)malloc(w * sizeof(float*));
-		if (A[i] == NULL) {
-			fprintf(stderr, "Out of memory");
-			exit(1);
-		}
+    for (int i = 0; i < h; i++)
+    {
+        data[i] = (float **)malloc(w * sizeof(float *));
+        if (data[i] == NULL)
+        {
+            printf("Out of memory");
+            exit(1);
+        }
 
-		for (int j = 0; j < w; j++)
-		{
-			A[i][j] = (float*)malloc(k * sizeof(float));
-				if (A[i][j] == NULL) {
-				fprintf(stderr, "Out of memory");
-				exit(1);
-			}
-		}
-	}
-	
-	/* assign values to allocated memory */
-	for (int i = 0; i < h; i++)
-		for (int j = 0; j < w; j++)
-			for (int z = 0; z < k; z++)
-				A[i][j][z] = v;
-                /* MADONNA. */
-    
+        for (int j = 0; j < w; j++)
+        {
+            data[i][j] = (float *)malloc(k * sizeof(float));
+            if (data[i][j] == NULL)
+            {
+                printf("Out of memory");
+                exit(1);
+            }
+        }
+    }
 
-    nuova->data = A;    
+    /* assign values to allocated memory */
+    for (int i = 0; i < h; i++)
+        for (int j = 0; j < w; j++)
+            for (int z = 0; z < k; z++)
+                data[i][j][z] = v;
+    /* MADONNA. */
+
+    nuova->data = data;
 
     return nuova;
+}
+
+/* Libera la memoria (data, stat e la struttura) */
+void ip_mat_free(ip_mat *a)
+{
+    int i, j, z;
+
+    /*Struct stat liberata.*/
+    free(a->stat);
+    printf("struttura statistiche liberata\n");
+
+    /*libero la matrice!*/
+    for (z = 0; i < a->k; z++)
+    {
+        for (i = 0; i < a->h; i++)
+        {
+            for (j = 0; j < a->w; j++)
+            {
+                free(&(a->data[i][j][z]));
+            }
+        }
+    }
+    printf("array liberato\n");
+    free(a->data);
+    printf("puntatore array liberato\n");
 }
 
 void ip_mat_show_stats(ip_mat *t)
@@ -272,63 +298,216 @@ void compute_stats(ip_mat *t)
 {
     int i;
 
-    for(i = 0; i < t->k; i++){
-    
-    t->stat[i].min = compute_min_data(t, t->h, t->w,i);
-    t->stat[i].max = compute_max_data(t, t->h, t->w, i);
-    t->stat[i].mean = compute_mean_data(t, t->h, t->w, i);
- 
+    for (i = 0; i < t->k; i++)
+    {
+
+        t->stat[i].min = compute_min_data(t, t->h, t->w, i);
+        t->stat[i].max = compute_max_data(t, t->h, t->w, i);
+        t->stat[i].mean = compute_mean_data(t, t->h, t->w, i);
     }
 }
 
-ip_mat *ip_mat_sum(ip_mat *a, ip_mat *b){  
-    
-    int i,j,z;
-    
-    /*creo una matrice di dimensione uguale ad A e la setto tutta 0*/
-    ip_mat *sum=ip_mat_create(a->h, a->w, a->k, 00.0);
-    
-    for(z=0;z<sum->k;z++){
-        for (i = 0; i < sum->h; i++)
+ip_mat *ip_mat_sum(ip_mat *a, ip_mat *b)
+{
+    /*VA FATTA UNA NUOVA MATRICE O NO??*/
+    /*VA FATTA UNA NUOVA MATRICE O NO??*/
+    /*VA FATTA UNA NUOVA MATRICE O NO??*/
+
+    int i, j, z;
+    if (a->h != b->h || a->w != b->w || a->k != b->k)
+    {
+        printf("Matrici diverse\n");
+        exit(1);
+    }
+    else
+    {
+        /*creo una matrice di dimensione uguale ad A e la setto tutta 0*/
+        ip_mat *sum = ip_mat_create(a->h, a->w, a->k, 00.0);
+
+        for (z = 0; z < sum->k; z++)
         {
-            for (j = 0; j < sum->w; j++)
+            for (i = 0; i < sum->h; i++)
             {
-                /*setto in posizione (scorri for) la somma della medesima posizione di *A e *B*/
-                set_val(sum,i,j,z,(get_val(a, i, j, z)+get_val(b, i, j, z)));
+                for (j = 0; j < sum->w; j++)
+                {
+                    /*setto in posizione (scorri for) la somma della medesima posizione di *A e *B*/
+                    set_val(sum, i, j, z, (get_val(a, i, j, z) + get_val(b, i, j, z)));
+                }
             }
         }
+        compute_stats(sum);
+        return sum;
     }
-    return sum;
 }
 
-ip_mat *ip_mat_sub(ip_mat *a, ip_mat *b){    
-    
-    int i,j,z;
-    
-    /*creo una matrice di dimensione uguale ad A e la setto tutta 0*/
-    ip_mat *sub=ip_mat_create(a->h, a->w, a->k, 00.0);
-    
-    for(z=0;z<sub->k;z++){
-        for (i = 0; i < sub->h; i++)
+ip_mat *ip_mat_sub(ip_mat *a, ip_mat *b)
+{
+    /*VA FATTA UNA NUOVA MATRICE O NO??*/
+    /*VA FATTA UNA NUOVA MATRICE O NO??*/
+    /*VA FATTA UNA NUOVA MATRICE O NO??*/
+
+    int i, j, z;
+    if (a->h != b->h || a->w != b->w || a->k != b->k)
+    {
+        printf("Matrici diverse\n");
+        exit(1);
+    }
+    else
+    {
+        /*creo una matrice di dimensione uguale ad A e la setto tutta 0*/
+        ip_mat *sub = ip_mat_create(a->h, a->w, a->k, 00.0);
+
+        for (z = 0; z < sub->k; z++)
         {
-            for (j = 0; j < sub->w; j++)
+            for (i = 0; i < sub->h; i++)
             {
-                /*setto in posizione (scorri for) la sottrazione della medesima posizione di *A e *B*/
-                set_val(sub,i,j,z,(get_val(a, i, j, z)-get_val(b, i, j, z)));
+                for (j = 0; j < sub->w; j++)
+                {
+                    /*setto in posizione (scorri for) la sottrazione della medesima posizione di *A e *B*/
+                    set_val(sub, i, j, z, (get_val(a, i, j, z) - get_val(b, i, j, z)));
+                }
             }
         }
+        compute_stats(sub);
+        return sub;
     }
-    return sub;
 }
 
+ip_mat *ip_mat_subset(ip_mat *t, unsigned int row_start, unsigned int row_end, unsigned int col_start, unsigned int col_end)
+{
+    int i, j, z;
 
+    if (row_start >= 0 && row_start <= row_end && row_end <= t->h && col_start >= 0 && col_start <= col_end && col_end < t->w)
+    {
+        ip_mat *sub_mat = ip_mat_create(row_end, col_end, t->k, 0);
+        /*NON SONO SICURO DEL -1 https://moodle.unive.it/mod/forum/discuss.php?d=63261 */
+        for (z = 0; z < sub_mat->k; z++)
+        {
+            for (i = 0; i < sub_mat->h; i++)
+            {
+                for (j = 0; j < sub_mat->w; j++)
+                {
+                    set_val(sub_mat, i, j, z, get_val(t, i, j, z));
+                }
+            }
+        }
+        return sub_mat;
+    }
+    else
+    {
+        printf("Valori non accettabili!\n");
+        exit(1);
+    }
+}
 
+ip_mat *ip_mat_concat(ip_mat *a, ip_mat *b, int dimensione)
+{
 
-ip_mat *ip_mat_copy(ip_mat *in);                                                                                              /*manuel*/
-ip_mat *ip_mat_subset(ip_mat *t, unsigned int row_start, unsigned int row_end, unsigned int col_start, unsigned int col_end); /*francesco*/
-ip_mat *ip_mat_concat(ip_mat *a, ip_mat *b, int dimensione);                                                                  /*francesco*/
+    int i, j, z;
+    ip_mat *concatenata;
 
+    if (dimensione == 0)
+    {
+        if (a->w == b->w && a->k == b->k)
+        {
+            concatenata = ip_mat_create(a->h + b->h, a->w, a->k, 0);
 
-ip_mat *ip_mat_mul_scalar(ip_mat *a, float c);                                                                                /*riccardo*/
-ip_mat *ip_mat_add_scalar(ip_mat *a, float c);                                                                                /*riccardo*/
-ip_mat *ip_mat_mean(ip_mat *a, ip_mat *b);                                                                                    /*manuel*/
+            for (z = 0; z < concatenata->k; z++)
+            {
+                for (i = 0; i < a->h; i++)
+                {
+                    for (j = 0; j < concatenata->w; j++)
+                    {
+                        set_val(concatenata, i, j, z, get_val(a, i, j, z));
+                    }
+                }
+                for (i = (a->h); i < concatenata->h; i++)
+                {
+                    for (j = 0; j < concatenata->w; j++)
+                    {
+                        set_val(concatenata, i, j, z, get_val(b, i - (a->h), j, z));
+                    }
+                }
+            }
+        }
+        else
+        {
+            printf("Non è possibile fare la concatenazione\n");
+            exit(1);
+        }
+    }
+    else if (dimensione == 1)
+    {
+        if (a->h == b->h && a->k == b->k)
+        {
+            concatenata = ip_mat_create(a->h, a->w + b->w, a->k, 0);
+
+            for (z = 0; z < concatenata->k; z++)
+            {
+                for (i = 0; i < a->h; i++)
+                {
+                    for (j = 0; j < a->w; j++)
+                    {
+                        set_val(concatenata, i, j, z, get_val(a, i, j, z));
+                    }
+
+                    for (j = a->w; j < concatenata->w; j++)
+                    {
+                        set_val(concatenata, i, j, z, get_val(b, i, j - (a->w), z));
+                    }
+                }
+            }
+        }
+
+        else
+        {
+            printf("Non è possibile fare la concatenazione\n");
+            exit(1);
+        }
+    }
+    else if (dimensione == 2)
+    {
+        if (a->h == b->h && a->w == b->w)
+        {
+            concatenata = ip_mat_create(a->h, a->w, a->k + b->k, 0);
+
+            for (z = 0; z < a->k; z++)
+            {
+                for (i = 0; i < a->h; i++)
+                {
+                    for (j = 0; j < concatenata->w; j++)
+                    {
+                        set_val(concatenata, i, j, z, get_val(a, i, j, z));
+                    }
+                }
+            }
+            for (z = a->k; z < concatenata->k; z++)
+            {
+                for (i = 0; i < a->h; i++)
+                {
+                    for (j = 0; j < concatenata->w; j++)
+                    {
+                        set_val(concatenata, i, j, z, get_val(b, i, j, z - (a->k)));
+                    }
+                }
+            }
+        }
+        else
+        {
+            printf("Non è possibile fare la concatenazione\n");
+            exit(1);
+        }
+    }
+    else
+    {
+        printf("Dimensione Errata\n");
+        exit(1);
+    }
+    return concatenata;
+}
+
+ip_mat *ip_mat_copy(ip_mat *in);           /*manuel*/
+ip_mat *ip_mat_mean(ip_mat *a, ip_mat *b); /*manuel*/
+
+ip_mat *ip_mat_mul_scalar(ip_mat *a, float c); /*riccardo*/
+ip_mat *ip_mat_add_scalar(ip_mat *a, float c); /*riccardo*/
